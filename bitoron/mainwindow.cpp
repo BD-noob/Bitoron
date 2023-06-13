1eaef3372b71db2c3a6eac262a3cd4ef1ae9687f
BD-noob <shahriar2alam7@gmail.com>
2023-06-13 15:48:22 +0600

    uploaded

---
+++ bitoron/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_Btn_signin_clicked()
+{
+    ui->container->setCurrentIndex(1);  //stackedWidget is renamed as container
+
+}
+
